dc355f8ae8768bb21501d6c7915918098ceb1cb5
joelcoxokc <joelcox@hisimagination.com>
2015-01-03 09:41:48 -0600

    Initial Demo Commit

---
+++ .tmp/scripts/core/config/routes.config.js
@@ -0,0 +1,23 @@
+;(function() {
+    'use strict';
+
+    angular
+        .module('core')
+        .config(Core);
+
+    /* @ngInject */
+    function Core ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/core/views/home.view.html',
+                controller:  'HomeController as vm'
+            });
+
+        ////////////////////////
+
+    }
+
+}).call(this);
---
+++ gulp/_client/app/core/core.module.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular.module('app.core', [
+        /*
+         * Angular modules
+         */
+        'ngAnimate', 'ngRoute', 'ngSanitize',
+        /*
+         * 3rd Party modules
+         */
+        'ngplus'
+    ]);
+})();
